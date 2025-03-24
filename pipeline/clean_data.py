03a9a3eb8612fe22205cbd1f04cfa035651cc467
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-03-24 20:35:50 +0530

    fix name issue

--- pipeline/clean_data.py
+++ pipeline/clean_data.py
@@ -5,9 +5,7 @@ class DataPreProcessingPipeline:
     def __init__(self):
         self.data_preprocessing = DataPreprocessing()
 
-    def start_cleaning(self, book, arvix):
-        clean_book_func = self.data_preprocessing.clean_book_data
-        clean_arvix_func = self.data_preprocessing.clean_arvix
+    def start_cleaning(self, book):
+        clean_book_func = self.data_preprocessing.clean_data
         book['chunk'] = book['chunk'].apply(clean_book_func)
-        arvix['chunk'] = arvix['chunk'].apply(clean_arvix_func)
-        return book, arvix
+        return book
